ab0890793c4b986e8f9c499995dd2dbe61352dfe
shravan-d <shravandinakaran@gmail.com>
2023-11-10 13:22:35 -0500

    initial commit

---
+++ detect.py
@@ -0,0 +1,68 @@
+import argparse
+import time
+from pathlib import Path
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+
+from models.experimental import attempt_load
+from utils.datasets import LoadImages, letterbox
+from utils.general import check_img_size, non_max_suppression, scale_coords
+
+
+def detect(source):
+    weights, imgsz,  = 'serverless/pytorch/custom-RL/nuclio/yolov7-updated-custom.pt', (640, 640)
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+
+    # Load model
+    model = attempt_load(weights)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+    # Set Dataloader
+    dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img)
+        img = img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Warmup
+
+        # Inference
+        pred = model(img)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, 0.25, 0.45)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+    print(det)
+    return det
+
+if __name__ == '__main__':
+    with torch.no_grad():
+        detect()
